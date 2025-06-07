01a1f54fdd189fc1ffdf5d9de811a0892b36ca4f
popka <drensymax@gmail.com>
2025-06-07 20:41:03 +0300

    MVC - паттерн. Разграничиваем обязанности контроллера - 3 (делаем контроллер для PosterActivity)

--- app/src/main/java/com/example/apitest2/Creator.kt
+++ app/src/main/java/com/example/apitest2/Creator.kt
@@ -1,13 +1,13 @@
 package com.example.apitest2
 
 import android.app.Activity
-import android.util.Log
 import com.example.apitest2.data.MoviesRepositoryImpl
 import com.example.apitest2.data.network.RetrofitNetworkClient
 import com.example.apitest2.domain.api.MoviesInteractor
 import com.example.apitest2.domain.api.MoviesRepository
 import com.example.apitest2.domain.impl.MoviesInteractorImpl
 import com.example.apitest2.presentation.MoviesSearchController
+import com.example.apitest2.presentation.PosterController
 import com.example.apitest2.ui.movies.MoviesAdapter
 
 
@@ -22,9 +22,12 @@ object Creator {
         return MoviesInteractorImpl(getMoviesRepository())
     }
 
-    // Добавим инициализацию контроллера в Creator:
     fun provideMoviesSearchController(activity: Activity, adapter: MoviesAdapter): MoviesSearchController {
         return MoviesSearchController(activity, adapter)
+    }
+
+    fun providePosterController(activity: Activity): PosterController {
+        return PosterController(activity)
     }
 
 
--- app/src/main/java/com/example/apitest2/ui/movies/MainActivity.kt
+++ app/src/main/java/com/example/apitest2/ui/movies/MainActivity.kt
@@ -5,27 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.view.View
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.apitest2.Creator
 import com.example.apitest2.ui.poster.PosterActivity
 import com.example.apitest2.R
 
-import com.example.apitest2.domain.api.MoviesInteractor
-import com.example.apitest2.domain.models.Movie
-
 
 class MainActivity : Activity() {
-
-    // А теперь научим MoviesActivity использовать новый контроллер:
 
     companion object {
         private const val CLICK_DEBOUNCE_DELAY = 1000L
@@ -69,11 +54,3 @@ class MainActivity : Activity() {
 
 }
 
-/*
-
-Так теперь выглядит MoviesActivity:
-- Перед методом onCreate() мы объявили и проинициализировали объект moviesSearchController, передав в метод инициализации экземпляр самой активити и адаптера.
-- В методах onCreate() и onDestroy() вызвали соответствующие методы контроллера.
-- В самой MoviesActivity осталась только логика перехода на экран с постером и обработка событий нажатия на элемент списка через debounce.
-
- */
